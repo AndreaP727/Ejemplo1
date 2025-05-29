1e5300b207a776158ff9365f373fdfa9e4384faa
AndreaP727 <andreaporras2009@gmail.com>
2025-05-28 21:25:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Declaracion y asignacion de variables con let
+let edad= 30;
+edad=31; // Se puede reasignar el valor de una variable declarada con let
+
+//Declaracion de variable constante
+const PI= 3.14;
+//PI= 3.14159; Esto provocara un error ya que no se puede reasignar una constante
+
+//Declaracion de variables con var
+var nombre="Juan";
+var nombre="Maria"; // se puede volver a declara con var
+
+console.log(edad); //salida:31
+console.log(PI);// Salida: 3.14
+console.log(nombre); //Salida: Maria
